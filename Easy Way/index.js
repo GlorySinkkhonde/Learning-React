a79c05482ca130a9dab088106c11e8384f12dbd4
Glory Sinkhonde <sinkhondeglory@gmail.com>
2022-10-31 16:51:51 +0200

    Learning React Notes & Code

---
+++ Easy Way/index.js
@@ -0,0 +1,45 @@
+//ReactDOM.render(<h1>I love REACT</h1>, document.getElementById("root"))
+
+/*
+
+html code can also be stored in a variable like below
+
+const list = (
+    <ul>
+
+    <li>
+        I love React
+    </li>
+    <li>
+        I love JavaScript
+    </li>
+    <li>
+        I love Node.Js
+    </li>
+
+</ul>
+)
+
+ReactDOM.render(
+list
+, document.getElementById("root"))*/
+
+//when the html is stored in a variable, it can get as nested as html with no problem
+
+const nav = (
+    <nav>
+
+     <h1>myBrand</h1>
+
+        <ul>
+            <li>pricing</li>
+            <li>about</li>
+            <li>contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    nav,
+    document.getElementById("root")
+)
