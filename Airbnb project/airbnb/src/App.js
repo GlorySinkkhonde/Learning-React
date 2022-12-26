44c889896551fbca9a1fcd8879d9bcf415829c64
Glory Sinkhonde <sinkhondeglory@gmail.com>
2022-12-26 12:17:18 +0200

    Card components using props

--- Airbnb project/airbnb/src/App.js
+++ Airbnb project/airbnb/src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Nav from './Components/Nav';
 import Hero from './Components/Hero';
 import Card from './Components/Card';
+import Sports from './images/sports.png'
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Hero />
-      <Card />
+      <Card 
+        img={Sports}
+        rating='5.0'
+        reviewCount='6'
+        country='USA'
+        title='Life lessons with Katie Zaferes'
+        price={136}
+      />
     </div>
   );
 }
--- Airbnb project/airbnb/src/Components/Card.jsx
+++ Airbnb project/airbnb/src/Components/Card.jsx
@@ -6,25 +6,46 @@ import MountainBike from '../images/mountain-bike.png'
 import { AiFillStar } from 'react-icons/ai'
 import { BsDot } from 'react-icons/bs'
 
-function Card() {
+function Card(props) {
   return (
     <div className='Container'>
         <div className="CardSection">
             <div className="Card">
-                <img src={SportsPhoto} alt="" />
-                <p> <span className='Stars'><AiFillStar /></span> <span>5.0</span> <span>(6) <BsDot className='Dot'/> </span> <span>USA</span></p>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><span>From $136</span> / person</p>
+                <img src={props.img} alt="" />
+                <p> <span className='Stars'><AiFillStar /></span> <span>{props.rating}</span> 
+                <span>({props.reviewCount}) • </span> 
+                <span>{props.country}</span></p>
+                <p>{props.title}</p>
+                <p>
+                <span>From ${props.price}</span> / person</p>
             </div>
             <div className="Card">
                 <img src={WeddingPhoto} alt="" />
-                <p> <span className='Stars'><AiFillStar /></span> 5.0 <span>(6)  <BsDot className='Dot'/> </span> <span>USA</span></p>
+                <p> <span className='Stars'><AiFillStar /></span> 5.0 <span>(6) • </span> <span>USA</span></p>
                 <p>Learn wedding photography</p>
                 <p><span>From $125</span> / person</p>
             </div>
             <div className="Card">
                 <img src={MountainBike} alt="" />
-                <p> <span className='Stars'><AiFillStar /></span> 5.0 <span>(6) <BsDot className='Dot' /> </span> <span>USA</span></p>
+                <p> <span className='Stars'><AiFillStar /></span> 5.0 <span>(6) • </span> <span>USA</span></p>
+                <p>Group Mountain Biking</p>
+                <p><span>From $50</span> / person</p>
+            </div>
+            <div className="Card">
+                <img src={SportsPhoto} alt="" />
+                <p> <span className='Stars'><AiFillStar /></span> <span>5.0</span> <span>(6) • </span> <span>USA</span></p>
+                <p>Life lessons with Katie Zaferes</p>
+                <p><span>From $136</span> / person</p>
+            </div>
+            <div className="Card">
+                <img src={WeddingPhoto} alt="" />
+                <p> <span className='Stars'><AiFillStar /></span> 5.0 <span>(6) • </span> <span>USA</span></p>
+                <p>Learn wedding photography</p>
+                <p><span>From $125</span> / person</p>
+            </div>
+            <div className="Card">
+                <img src={MountainBike} alt="" />
+                <p> <span className='Stars'><AiFillStar /></span> 5.0 <span>(6) • </span> <span>USA</span></p>
                 <p>Group Mountain Biking</p>
                 <p><span>From $50</span> / person</p>
             </div>
